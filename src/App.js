8a9608c5e051fac6b6fe50226eedfd74151af5f9
İlke Eren Demirtaş <demirtas.eren@outlook.com>
2022-03-23 13:51:15 +0300

    Error 404 Sayfası geliyor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
-import {Box} from "@chakra-ui/react";
+import Error404 from "./pages/Error404";
+import { Box } from "@chakra-ui/react";
+import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Box h={{base: "100%", lg:"100vh"}} bgColor={"#1c1c1c"}>
+    <div>
+      <Box h={{ base: "100%", lg: "100vh" }} bgColor={"#1c1c1c"}>
         <Home />
       </Box>
-    </>
+    </div>
   );
 }
 
